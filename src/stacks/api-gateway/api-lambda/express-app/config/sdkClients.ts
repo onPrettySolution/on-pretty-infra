e6c7ac4a34197e37b6ed4987155b2e19311eff36
Vasyl Herman 🌥 <vasil.german@gmail.com>
2025-06-15 21:29:55 +0300

    optimise sdk client helpers

--- src/stacks/api-gateway/api-lambda/express-app/config/sdkClients.ts
+++ src/stacks/api-gateway/api-lambda/express-app/config/sdkClients.ts
@@ -3,19 +3,17 @@ import {DynamoDBDocumentClient} from '@aws-sdk/lib-dynamodb';
 import {CognitoIdentityClient} from "@aws-sdk/client-cognito-identity";
 
 // create docClient
-let dynamoDBDocumentClient: DynamoDBDocumentClient | null = null;
+let _docClientInstance: DynamoDBDocumentClient;
 const getDocClient = (): DynamoDBDocumentClient => {
-    if (dynamoDBDocumentClient) return dynamoDBDocumentClient;
-    console.log(`[WARN ${Date.now()}]:`, 'new DynamoDBClient()');
-    // console.log(`[WARN ${nanoid()}]:`, 'new DynamoDBClient()');
-    dynamoDBDocumentClient = DynamoDBDocumentClient.from(new DynamoDBClient());
-    return dynamoDBDocumentClient;
-};
-export const docClient = getDocClient();
+    return _docClientInstance ??= DynamoDBDocumentClient.from(new DynamoDBClient());
+}
 
 // create cognito identity client
-let _cognitoIdentityClient: CognitoIdentityClient | null = null;
-export const getCognitoIdentityClient = (): CognitoIdentityClient => {
+let _cognitoIdentityClient: CognitoIdentityClient;
+const getCognitoIdentityClient = (): CognitoIdentityClient => {
     return _cognitoIdentityClient ??= new CognitoIdentityClient();
 };
+
+// export
 export const cognitoIdentityClient = getCognitoIdentityClient()
+export const docClient = getDocClient();
